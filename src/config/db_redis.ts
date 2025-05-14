e7ebca996a21f099e5651d02003986070e6164ad
fernandobrandan <fernando.brandan1@hotmail.com>
2025-05-14 10:45:41 -0300

    update

--- src/config/db_redis.ts
+++ src/config/db_redis.ts
@@ -1,8 +1,8 @@
 import Redis from 'ioredis'
-const url = process.env.REDIS_URL || 'redis://redis_cache:6379/0';
-const redisClient = new Redis({ host: 'redis_cache', port: 6379 });
+//const url = process.env.REDIS_URL || 'redis://redis_cache:6379/0';
+const redisClient = new Redis({ host: 'redis_cache', port: 6379 })
 redisClient.on('connect', () => { console.log('Redis client connected') })
 redisClient.on('ready', () => { console.log('Redis client ready') })
-redisClient.on("error", (err) => console.error("Error:", err));
-redisClient.on("end", () => console.log("Conexión cerrada"));
-export default redisClient;
+redisClient.on("error", (err) => console.error("Error:", err))
+redisClient.on("end", () => console.log("Conexión cerrada"))
+export default redisClient
